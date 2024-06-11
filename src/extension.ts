80f250d36c0ba664365d13f03ad95195d8a281d7
mario robayo <marioferyhwh@gmail.com>
2024-06-11 17:16:30 -0500

    corrige estructura

--- src/extension.ts
+++ src/extension.ts
@@ -4,43 +4,37 @@ import * as vscode from 'vscode';
 import { formatDocument, applyEdits } from './formatterUtils';
 
 export function activate(context: vscode.ExtensionContext) {
-  const  LANGUAGE_ID="any-lang";
-  const  COMMAND_FORMAT="extension.format-any";
+  const LANGUAGE_ID = "any-lang";
+  const COMMAND_FORMAT = "extension.format-any";
     
-    // 👎 formatter implemented as separate command
-    vscode.commands.registerCommand(COMMAND_FORMAT, () => {
-      
-        vscode.window.showInformationMessage("evento "+COMMAND_FORMAT);
-        const {activeTextEditor} = vscode.window;
+  // 👎 Formatter implemented as separate command
+  vscode.commands.registerCommand(COMMAND_FORMAT, () => {
+    vscode.window.showInformationMessage("Evento " + COMMAND_FORMAT);
+    const { activeTextEditor } = vscode.window;
 
-        if (activeTextEditor) {
-            const { document } = activeTextEditor;
-            const edits = formatDocument(document);
-            applyEdits(document, edits);
-        }
-    });
-
-    // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider(LANGUAGE_ID, {
-      provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-        vscode.window.showInformationMessage("Formateador de  "+LANGUAGE_ID);
-        return formatDocument(document);
-      }
+    if (activeTextEditor) {
+      const { document } = activeTextEditor;
+      const edits = formatDocument(document);
+      applyEdits(document, edits);
+    }
   });
 
-
-
-    // Escuchar el evento de guardado de documentos
-    vscode.workspace.onDidSaveTextDocument(document => {
-      vscode.window.showInformationMessage("Se detecta guardado");
-      if (document.languageId !== LANGUAGE_ID &&  document.fileName.endsWith('.foo')) {
-        vscode.commands.executeCommand(COMMAND_FORMAT);
-      }
+  // 👍 Formatter implemented using API
+  vscode.languages.registerDocumentFormattingEditProvider(LANGUAGE_ID, {
+    provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+      vscode.window.showInformationMessage("Formateador de " + LANGUAGE_ID);
+      return formatDocument(document);
+    }
   });
-  
+
+  // Escuchar el evento de guardado de documentos
+  vscode.workspace.onDidSaveTextDocument(document => {
+    vscode.window.showInformationMessage("Se detecta guardado");
+    if (document.languageId !== LANGUAGE_ID && document.fileName.endsWith('.foo')) {
+      const edits = formatDocument(document);
+      applyEdits(document, edits);
+    }
+  });
 }
 
-
-
 export function deactivate() {}
-
