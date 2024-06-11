b9625efe47dba35706cf404621cef52bfcadd8fe
mario robayo <marioferyhwh@gmail.com>
2024-06-11 15:02:14 -0500

    se modifica para validar con if
    se generaliza  a any
    
    establecer constantes
    
    se mueve logica  anuevo archivo  y se  m modifican mensajes
    
    corrige estructura
    
    se configura para que se active al niciar para las pruebas
    
    otra  test
    
    realiza identado
    
    modifia  codigo
    
    mejora foramto
    
    se agregan espacios
    
    modifia ext
    
    modifcia archivo test
    
    add code
    
    se agrga identacion
    
    seagrega  ;
    
    se modifica identacion
    
    change names
    
    mueve a devdependecies
    
    add properties
    
    add file
    
    change names
    
    corrige comando y  se actualiza datos d econfig
    
    add distingir  XData
    
    identacion
    
    ser configura identacion en XData
    
    modidifa formateos
    
    update
    
     test files
    
    testt file
    
    change format
    
    add test
    
    auto save
    
    formatea codigo

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,43 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { formatDocument, applyEdits } from './formatterUtils';
 
 export function activate(context: vscode.ExtensionContext) {
+  const LANGUAGE_ID = "objectscript";
+  const COMMAND_FORMAT = "extension.formatObjectScript";
 
-    // 👎 formatter implemented as separate command
-    vscode.commands.registerCommand('extension.format-foo', () => {
-        const {activeTextEditor} = vscode.window;
+    
+  // 👎 Formatter implemented as separate command
+  vscode.commands.registerCommand(COMMAND_FORMAT, async() => {
+    vscode.window.showInformationMessage("Evento " + COMMAND_FORMAT);
+    const { activeTextEditor } = vscode.window;
 
-        if (activeTextEditor && activeTextEditor.document.languageId === 'foo-lang') {
-            const {document} = activeTextEditor;
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                const edit = new vscode.WorkspaceEdit();
-                edit.insert(document.uri, firstLine.range.start, '42\n');
-                return vscode.workspace.applyEdit(edit)
-            }
-        }
-    });
+    if (activeTextEditor) {
+      const { document } = activeTextEditor;
+      const edits = formatDocument(document);
+      await applyEdits(document, edits);
+      await document.save();
+    }
+  });
 
-    // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider('foo-lang', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
-            }
-        }
-    });
+  // 👍 Formatter implemented using API
+  vscode.languages.registerDocumentFormattingEditProvider(LANGUAGE_ID, {
+    provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+      vscode.window.showInformationMessage("Formateador de " + LANGUAGE_ID);
+      return formatDocument(document);
+    }
+  });
+
+  // Escuchar el evento de guardado de documentos
+  vscode.workspace.onDidSaveTextDocument(async (document) => {
+    vscode.window.showInformationMessage("Se detecta guardado");
+    if (document.languageId !== LANGUAGE_ID && document.fileName.endsWith('.foo')) {
+      //const edits = formatDocument(document);
+      //await applyEdits(document, edits);
+      //await document.save();
+    }
+  });
 }
 
-
+export function deactivate() {}
---
+++ src/formatterUtils.ts
@@ -0,0 +1,197 @@
+
+import * as vscode from 'vscode';
+
+
+let charIdent=" "
+let numberCharIdent=2
+
+// Función para aplicar ediciones al documento
+export const applyEdits = async (document: vscode.TextDocument, edits: vscode.TextEdit[]) => {
+  if (edits.length > 0) {
+    const workspaceEdit = new vscode.WorkspaceEdit();
+    edits.forEach(edit => workspaceEdit.replace(document.uri, edit.range, edit.newText));
+    await vscode.workspace.applyEdit(workspaceEdit);
+  }
+}
+
+function UpdateConfigBase(){
+  // Obtener configuraciones de usuario
+  const config = vscode.workspace.getConfiguration('formatterSample');
+  numberCharIdent = config.get<number>('indentationSize', 2);
+  const indentationType = config.get<string>('indentationType', 'space');
+  if (indentationType == "space"){
+    charIdent = " ";
+  }else{
+    charIdent = "\t";
+  }
+}
+
+const formaBaseLine = (textLine: string):string =>{
+  let nuwTextLine = textLine;
+  //agrega espacios en logica
+  nuwTextLine = nuwTextLine.replace(/(elseif|if|else|try|catch|while|for|switch)\s*\(\s*([^{]*)\s*\)\s*/gi, '$1 ( $2 ) ');
+  nuwTextLine = nuwTextLine.replace(/\}\s*(catch|elseif|else)\s*/gi, '} $1 ');
+  nuwTextLine = nuwTextLine.replace(/\s*(try)\s*\{/gi, '$1 {');
+  //validar if quit:condition
+  const pointCondition = textLine.match(/^[ \t]*(quit):(\s*([^\n]*)\s*)$/);
+  let expectSpaceOpetator = ' $1 ';
+  if (pointCondition != null){
+    expectSpaceOpetator = '$1';
+  }
+  //modificar espacio en operadores
+  nuwTextLine = nuwTextLine.replace(/\s*(>=|<=|'=|>|<|=)\s*/ig, expectSpaceOpetator);
+  //separacion (a,b,    b   )=>(a, b, b)
+  nuwTextLine = nuwTextLine.replace(/(?<=\()([^)]+)(?=\))/g, (match) => {
+    return match.replace(/\s*,\s*/g, ", ");
+  });
+
+  // Elimnar ;
+  nuwTextLine = nuwTextLine.replace(/^\s*((set|quit|write|do)[^\n;]*)[;\s]*$/ig, '$1')
+  // Eliminar espacios dobles y al final del renglón
+  nuwTextLine = nuwTextLine.replace(/\s{2,}/g, ' ').replace(/\s+$/g, '');
+  // Eliminar líneas que contienen solo espacios o tabs
+  nuwTextLine = nuwTextLine.replace(/^[ \t]+$/gm, '');
+  return nuwTextLine;
+}
+
+
+
+const getIdentationLevel = (textLine: string):number => {
+  const textIdent = textLine.match(/^([ \t]*)/);
+  if (!textIdent) {
+    return 0;
+  }
+  const arrayIdent = textIdent[1].match(/([ \t])/g);
+  if (!arrayIdent){
+    return 0;
+  }
+  return arrayIdent.length/numberCharIdent;
+}
+
+const getTextIdent = (indentationLevel:number):string =>{
+  if (indentationLevel <= 0){
+    return "";
+  }
+  return charIdent.repeat(indentationLevel*numberCharIdent);
+}
+
+const addIdentado = (textLine: string,indentationLevel :number = 0):string =>{
+  if (!textLine){
+    return "";
+  }
+
+  return getTextIdent(indentationLevel) + textLine;
+}
+
+// Función para limpiar y formatear las líneas del documento
+const cleanAndFormatLines = (document: vscode.TextDocument): string[] => {
+  const formattedLines: string[] = [];
+  let insideXData = false;
+  let indentationLevel = -1;
+
+  for (let i = 0; i < document.lineCount; i++) {
+    const line = document.lineAt(i);
+    const trimmedLine = line.text.trim();
+
+    if (trimmedLine.match(/^(XData|Storage)\s+\w+/)) {
+      insideXData = true;
+    }
+    
+    let OpenXdata = 0;
+    let CloseXdata = 0;
+    if (insideXData){
+      OpenXdata = trimmedLine.match(/<[^>\/]+>/g)?.length || 0;
+      CloseXdata = trimmedLine.match(/<\/[^>]+>/g)?.length || 0;
+    }
+    let OpenBreak = trimmedLine.match(/\{/g)?.length || 0;
+    let CloseBreak = trimmedLine.match(/\}/g)?.length || 0;
+    
+    const numberOpen = OpenBreak + OpenXdata;
+    const numberClosed = CloseBreak + CloseXdata;
+
+    const formattedLine = insideXData ? trimmedLine : formaBaseLine(trimmedLine);
+
+    // Ajustar el nivel de indentación según las llaves de cierre
+    if (numberClosed && !(OpenXdata && CloseXdata)) {
+      indentationLevel = indentationLevel - 1;
+    }
+
+    // Ajustar la indentación adecuada
+    const indentedLine = addIdentado(formattedLine,indentationLevel);
+
+    
+    // Ajustar el nivel de indentación según las llaves de cierre
+    if (numberClosed && (OpenXdata && CloseXdata)) {
+      indentationLevel = indentationLevel - 1;
+    }
+
+    if (numberClosed > 1) {
+      indentationLevel = indentationLevel - numberClosed + 1;
+    }
+
+    // Ajustar el nivel de indentación según las llaves de apertura
+    if (numberOpen) {
+      indentationLevel++;
+    }
+    
+    if (insideXData && trimmedLine.match(/^}/)) {
+      insideXData = false;
+    }
+
+    formattedLines.push(indentedLine);
+  }
+  
+  return formattedLines;
+}
+
+const  clearCode =  (text: string):string =>{
+  let nuwText = text;
+
+  //agregar saltos de liena cuando seencuentran }}
+  nuwText = nuwText.replace(/^([ \t]*)([^\}\n]*?)(}}+)/gm,(match, p1, p2, p3) => {
+    let text = p1 + p2;
+    let identationLevel = getIdentationLevel(p1)
+    text = p3.split('').reduce( (prev,dat,index)=>{
+      if (index == 0){
+        return  prev + dat;
+      }
+      identationLevel--;
+      return prev +"\n"+getTextIdent(identationLevel)+dat;
+    },text);
+    return text;
+  });
+
+  // Quitar saltos de liena después de {
+  nuwText = nuwText.replace(/(\{)\n{2,}/g, '$1\n');
+
+  // Quitar renglones vacíos antes de }
+  nuwText = nuwText.replace(/\n{2,}(\s*\})/g, '\n$1');
+
+  // Eliminar saltos de línea consecutivos si hay más de uno
+  nuwText = nuwText.replace(/\n{3,}/g, '\n\n');
+
+  // Eliminar saltos de línea dobles al final del documento, dejar solo uno
+  nuwText = nuwText.replace(/\n{2,}$/g, '\n');
+
+  return nuwText;
+}
+
+// Función para formatear un documento
+export const formatDocument = (document: vscode.TextDocument): vscode.TextEdit[] => {
+  UpdateConfigBase();
+  const edits: vscode.TextEdit[] = [];
+  const lines = cleanAndFormatLines(document);
+  let formattedText = lines.join('\n');
+
+  // Agregar saltos de línea a los corchetes
+  formattedText = formattedText.replace(/^([^\n\/\/]*\{)/gm, '$1\n');
+
+  formattedText = clearCode(formattedText);
+
+  // Reemplazar todo el contenido del documento con el texto formateado
+  const lastLine = document.lineAt(document.lineCount - 1);
+  const range = new vscode.Range(0, 0, lastLine.range.end.line, lastLine.range.end.character);
+  edits.push(vscode.TextEdit.replace(range, formattedText));
+
+  return edits;
+};
