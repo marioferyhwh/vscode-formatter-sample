5c7e481d45552bf259202b378d1b169703dd4543
mario robayo <marioferyhwh@gmail.com>
2024-06-11 19:37:50 -0500

    realiza identado

--- src/formatterUtils.ts
+++ src/formatterUtils.ts
@@ -13,6 +13,7 @@ export const applyEdits = (document: vscode.TextDocument, edits: vscode.TextEdit
 export const formatDocument = (document: vscode.TextDocument): vscode.TextEdit[] => {
   const edits: vscode.TextEdit[] = [];
   let formattedText = '';
+  let indentationLevel = 0;
 
   // Recorrer todas las líneas del documento
   for (let i = 0; i < document.lineCount; i++) {
@@ -21,15 +22,38 @@ export const formatDocument = (document: vscode.TextDocument): vscode.TextEdit[]
 
       // Formatear la línea eliminando espacios dobles y espacios al final del renglón
       const formattedLine = trimmedLine.replace(/\s{2,}/g, ' ').replace(/\s+$/g, '');
-      
 
-      // Agregar espacios al final del documento
-      formattedText += formattedLine + (i < document.lineCount - 1 ? '\n\n' : '\n');
+      // Agregar la indentación adecuada
+      formattedText += '\t'.repeat(indentationLevel) + formattedLine;
+
+      // Determinar si la línea abre o cierra un bloque
+      const openingBrace = /\{$/.test(trimmedLine);
+      const closingBrace = /^\}$/.test(trimmedLine);
+
+      // Ajustar el nivel de indentación según las llaves de apertura y cierre
+      if (openingBrace) {
+          indentationLevel++;
+      } else if (closingBrace) {
+          indentationLevel = Math.max(0, indentationLevel - 1);
+      }
+
+      // Agregar un salto de línea después de cada línea, excepto la última
+      formattedText += '\n';
   }
-
-  formattedText = formattedText.replace(/\{/g, '{\n');
+  //\{|\(|\[
+  //agregar saltos de linea a los corchetes
+  formattedText = formattedText.replace(/(\{)/g, '$1\n');
+  formattedText = formattedText.replace(/(\})/g, '\n$1\n');
+  // las de solo espacios o tabs son limpiadas 
+  formattedText = formattedText.replace(/^[ \t]+$/gm, '');
+  //se quitan espacios vacios despues de las {
+  formattedText = formattedText.replace(/(\{)\n{2,}/g, '$1\n');
+  //se quitan renglones vaciona antes de }
+  formattedText = formattedText.replace(/\n{2,}(\})/g, '\n\$1');
   // Eliminar saltos de línea consecutivos si hay más de uno
   formattedText = formattedText.replace(/\n{3,}/g, '\n\n');
+  // elimina si hay mas de 2 saltos de linea al final
+  formattedText = formattedText.replace(/\n{2,}$/g, '\n');
 
   // Reemplazar todo el contenido del documento con el texto formateado
   const lastLine = document.lineAt(document.lineCount - 1);
