74e7bed8bbf9226b8126aa0e4583ca75f6064288
marioF <marioferyhwh@gmail.com>
2024-06-22 07:51:02 -0500

    format
    modif ident
    
    mueve codigo a funcion
    
    order code
    
    ordena codigo
    
    use function
    
    moved code
    
    agrega logica para ignorar  comentarios
    
    se cambia lugar de donde saca los parametro de configuracion
    
    se modifica unicaon de archivos de extension
    
    modif test
    
    add comment
    
    rename and comment
    
    recatoriza
    
    config to save
    
    modifica  try y catch
    
    se identifica campos apr ano identasr y no formatear
    
    sube version

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,53 @@
-'use strict';
+//extension.ts
+"use strict";
 
-import * as vscode from 'vscode';
-import { formatDocument, applyEdits } from './formatterUtils';
+import * as vscode from "vscode";
+import { formatDocument, applyEdits } from "./formatterUtils";
 
 export function activate(context: vscode.ExtensionContext) {
-  const LANGUAGE_ID = "objectscript";
+  const LANGUAGE_ID = "objectscript-class";
   const COMMAND_FORMAT = "extension.formatObjectScript";
 
-    
+  const executeFormatter = async (document: vscode.TextDocument) => {
+    vscode.window.showInformationMessage("executeFormatter");
+    const edits = formatDocument(document);
+    await applyEdits(document, edits);
+    await document.save();
+  }
   // 👎 Formatter implemented as separate command
-  vscode.commands.registerCommand(COMMAND_FORMAT, async() => {
+  vscode.commands.registerCommand(COMMAND_FORMAT, async () => {
     vscode.window.showInformationMessage("Evento " + COMMAND_FORMAT);
     const { activeTextEditor } = vscode.window;
 
     if (activeTextEditor) {
       const { document } = activeTextEditor;
-      const edits = formatDocument(document);
-      await applyEdits(document, edits);
-      await document.save();
+      await executeFormatter(document);
     }
   });
 
   // 👍 Formatter implemented using API
   vscode.languages.registerDocumentFormattingEditProvider(LANGUAGE_ID, {
-    provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument
+    ): vscode.TextEdit[] {
       vscode.window.showInformationMessage("Formateador de " + LANGUAGE_ID);
       return formatDocument(document);
-    }
+    },
   });
 
   // Escuchar el evento de guardado de documentos
   vscode.workspace.onDidSaveTextDocument(async (document) => {
+    const config = vscode.workspace.getConfiguration("FormatterObjectScript");
+    const formattingOnSave = config.get<boolean>("enableFormattingOnSave", true);
+    const supportedExtensions = config.get<string[]>("supportedExtensionsOnSave", []);
+    const fileExtension = document.fileName.split('.').pop();
+    const validExtension = fileExtension && supportedExtensions.includes(`.${fileExtension}`)
+    
     vscode.window.showInformationMessage("Se detecta guardado");
-    if (document.languageId !== LANGUAGE_ID && document.fileName.endsWith('.foo')) {
-      //const edits = formatDocument(document);
-      //await applyEdits(document, edits);
-      //await document.save();
+    if (formattingOnSave && (document.languageId == LANGUAGE_ID || validExtension)) {
+      await executeFormatter(document);
     }
   });
 }
 
-export function deactivate() {}
+export function deactivate() { }
