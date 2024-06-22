62d65023d4fa67b4e0f08c1bd4a4a009e7614141
marioF <marioferyhwh@gmail.com>
2024-06-22 07:51:02 -0500

    format
    modif ident
    
    mueve codigo a funcion
    
    order code
    
    ordena codigo
    
    use function
    
    moved code
    
    agrega logica para ignorar  comentarios
    
    se cambia lugar de donde saca los parametro de configuracion
    
    se modifica unicaon de archivos de extension
    
    modif test
    
    add comment
    
    rename and comment
    
    recatoriza
    
    config to save
    
    modifica  try y catch
    
    se identifica campos apr ano identasr y no formatear
    
    sube version

--- src/formatterUtils.ts
+++ src/formatterUtils.ts
@@ -1,196 +1,288 @@
+//formatterUtils.ts
+"use strict";
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-
-let charIdent=" "
-let numberCharIdent=2
+let charIndent = " ";
+let numberCharIndent = 2;
 
 // Función para aplicar ediciones al documento
-export const applyEdits = async (document: vscode.TextDocument, edits: vscode.TextEdit[]) => {
+export const applyEdits = async (
+  document: vscode.TextDocument,
+  edits: vscode.TextEdit[]
+) => {
   if (edits.length > 0) {
     const workspaceEdit = new vscode.WorkspaceEdit();
-    edits.forEach(edit => workspaceEdit.replace(document.uri, edit.range, edit.newText));
+    edits.forEach((edit) =>
+      workspaceEdit.replace(document.uri, edit.range, edit.newText)
+    );
     await vscode.workspace.applyEdit(workspaceEdit);
   }
-}
+};
 
-function UpdateConfigBase(){
-  // Obtener configuraciones de usuario
-  const config = vscode.workspace.getConfiguration('formatterSample');
-  numberCharIdent = config.get<number>('indentationSize', 2);
-  const indentationType = config.get<string>('indentationType', 'space');
-  if (indentationType == "space"){
-    charIdent = " ";
-  }else{
-    charIdent = "\t";
-  }
+// Actualizar configuraciones base
+const updateConfigBase = () =>{
+  const config = vscode.workspace.getConfiguration("FormatterObjectScript");
+  numberCharIndent = config.get<number>("indentationSize", 0);
+  const useSpaces = config.get<boolean>("useSpacesForIndentation", true);
+  charIndent = useSpaces ? " " : "\t";
 }
+// Formatear una línea de código
+const formatCodeInLine = (textLine: string): string => {
+  if (!textLine) return textLine;
 
-const formaBaseLine = (textLine: string):string =>{
-  let nuwTextLine = textLine;
+  let newTextLine  = textLine;
   //agrega espacios en logica
-  nuwTextLine = nuwTextLine.replace(/(elseif|if|else|try|catch|while|for|switch)\s*\(\s*([^{]*)\s*\)\s*/gi, '$1 ( $2 ) ');
-  nuwTextLine = nuwTextLine.replace(/\}\s*(catch|elseif|else)\s*/gi, '} $1 ');
-  nuwTextLine = nuwTextLine.replace(/\s*(try)\s*\{/gi, '$1 {');
+  newTextLine  = newTextLine .replace(
+    /(elseif|if|else|try|for|switch)\s*\(\s*([^{]*)\s*\)/gi,
+    "$1 ( $2 ) "
+  );
+  newTextLine  = newTextLine .replace(
+    /(catch|while)\s*\(\s*([^{]*)\s*\)/gi, (match, p1, p2)=>{
+      return `${p1} (${p2.trim()})`;
+    }
+  );
+  newTextLine  = newTextLine .replace(/\}\s*(catch|elseif|else)\s*/gi, "} $1 ");
+  newTextLine  = newTextLine .replace(/\s*(try)\s*\{/gi, "$1 {");
   //validar if quit:condition
   const pointCondition = textLine.match(/^[ \t]*(quit):(\s*([^\n]*)\s*)$/);
-  let expectSpaceOpetator = ' $1 ';
-  if (pointCondition != null){
-    expectSpaceOpetator = '$1';
-  }
+  const expectSpaceOperator = pointCondition ? "$1" : " $1 ";
+
   //modificar espacio en operadores
-  nuwTextLine = nuwTextLine.replace(/\s*(>=|<=|'=|>|<|=)\s*/ig, expectSpaceOpetator);
+  newTextLine  = newTextLine .replace(
+    /\s*(>=|<=|'=|>|<|=)\s*/gi,
+    expectSpaceOperator
+  );
   //separacion (a,b,    b   )=>(a, b, b)
-  nuwTextLine = nuwTextLine.replace(/(?<=\()([^)]+)(?=\))/g, (match) => {
+  newTextLine  = newTextLine .replace(/(?<=\()([^)]+)(?=\))/g, (match) => {
     return match.replace(/\s*,\s*/g, ", ");
   });
 
   // Elimnar ;
-  nuwTextLine = nuwTextLine.replace(/^\s*((set|quit|write|do)[^\n;]*)[;\s]*$/ig, '$1')
+  newTextLine  = newTextLine .replace(
+    /^\s*((set|quit|write|do)[^\n;]*)[;\s]*$/gi,
+    "$1"
+  );
   // Eliminar espacios dobles y al final del renglón
-  nuwTextLine = nuwTextLine.replace(/\s{2,}/g, ' ').replace(/\s+$/g, '');
+  newTextLine  = newTextLine .replace(/\s{2,}/g, " ").replace(/\s+$/g, "");
   // Eliminar líneas que contienen solo espacios o tabs
-  nuwTextLine = nuwTextLine.replace(/^[ \t]+$/gm, '');
-  return nuwTextLine;
-}
+  newTextLine  = newTextLine .replace(/^[ \t]+$/gm, "");
+  return newTextLine ;
+};
 
+// Formatear base de una línea
+const formatBaseLine = (textLine: string): string => {
+  return textLine.replace(/^([^\n]*?)(?=\/\/|$)(\/\/.*)?$/, (match, code = "", comment1 = "") => {
+    return formatCodeInLine(code) + comment1;
+  });
+};
 
-
-const getIdentationLevel = (textLine: string):number => {
+// Formatear base de una línea
+const getIndentationLevel = (textLine: string): number => {
   const textIdent = textLine.match(/^([ \t]*)/);
   if (!textIdent) {
     return 0;
   }
   const arrayIdent = textIdent[1].match(/([ \t])/g);
-  if (!arrayIdent){
+  if (!arrayIdent) {
     return 0;
   }
-  return arrayIdent.length/numberCharIdent;
-}
+  return arrayIdent.length / numberCharIndent;
+};
 
-const getTextIdent = (indentationLevel:number):string =>{
-  if (indentationLevel <= 0){
+// Obtener texto indentado
+const getTextIndent = (indentationLevel: number): string => {
+  if (indentationLevel <= 0) {
     return "";
   }
-  return charIdent.repeat(indentationLevel*numberCharIdent);
-}
+  return charIndent.repeat(indentationLevel * numberCharIndent);
+};
 
-const addIdentado = (textLine: string,indentationLevel :number = 0):string =>{
-  if (!textLine){
+// Añadir indentación
+const addIndentation = (
+  textLine: string,
+  indentationLevel: number = 0
+): string => {
+  if (!textLine) {
     return "";
   }
 
-  return getTextIdent(indentationLevel) + textLine;
+  return getTextIndent(indentationLevel) + textLine;
+};
+
+// Detectar XML
+const detectXml = (text: string): [number, number] => {
+  const OpenXmle = text.match(/(?!<!)<[^>\/]+>/g)?.length || 0;
+  const CloseXmle = text.match(/<\/[^>]+>/g)?.length || 0;
+  return [OpenXmle, CloseXmle];
 }
 
-// Función para limpiar y formatear las líneas del documento
+// Detectar llaves
+const detectBrace = (text: string): [number, number] => {
+  const OpenBrace = text.match(/\{/g)?.length || 0;
+  const CloseBrace = text.match(/\}/g)?.length || 0;
+  return [OpenBrace, CloseBrace];
+}
+
+// Limpiar y formatear líneas del documento
 const cleanAndFormatLines = (document: vscode.TextDocument): string[] => {
   const formattedLines: string[] = [];
   let insideXData = false;
+  let insidetOff = false;
   let indentationLevel = -1;
+  let oldTextCode = "";
+  let commitOpen = false;
+
+  const outTestCode = (text: string): string => {
+    let textOut = "";
+    if (!commitOpen) {
+      const textWithoutComment1 = text.match(/^([^\n]*?)(?=\/\/|$)/)[0] ?? "";
+      const textWithoutComment2 = textWithoutComment1.replace(/\/\*.*?\*\//gm, "");
+      const textWithoutComment3 = textWithoutComment2.replace(/^(.*?)(?=\/\*)\/\*.*?$/, "$1");
+      if (textWithoutComment3 != textWithoutComment2) {
+        commitOpen = true;
+      }
+      textOut = textWithoutComment3;
+    } else {
+      const textWithoutComment1 = text.replace(/^.*?(?=\*\/)\*\/(.*?)$/gm, "$1");
+      if (textWithoutComment1 != text) {
+        commitOpen = false;
+      }
+      if (commitOpen) {
+        textOut = "";
+      } else {
+        const textWithoutComment2 = textWithoutComment1.match(/^([^\n]*?)(?=\/\/|$)/)[0] ?? "";
+        const textWithoutComment3 = textWithoutComment2.replace(/\/\*.*?\*\//gm, "");
+        const textWithoutComment4 = textWithoutComment3.replace(/^(.*?)(?=\/\*)\/\*.*?$/, "$1");
+        if (textWithoutComment3 != textWithoutComment4) {
+          commitOpen = true;
+        }
+        textOut = textWithoutComment4;
+      }
+    }
+    return textOut;
+  }
+
+  const calculateIndentation = (trimmedLine: string): void => {
+
+    const [oldOpenXml, oldCloseXml] = insideXData ? detectXml(oldTextCode) : [0, 0];
+    const [oldOpenBrace, oldCloseBrace] = detectBrace(oldTextCode);
+    const oldNumberOpen = oldOpenBrace + oldOpenXml;
+    const oldNumberClosed = oldCloseBrace + oldCloseXml;
+
+    // Ajustar el nivel de indentación según las llaves de cierre
+    if (oldNumberClosed && oldOpenXml && oldCloseXml) {
+      indentationLevel = indentationLevel - 1;
+    }
+
+    if (oldNumberClosed > 1) {
+      indentationLevel = indentationLevel - oldNumberClosed + 1;
+    }
+
+    // Ajustar el nivel de indentación según las llaves de apertura
+    if (oldNumberOpen) {
+      indentationLevel++;
+    }
+
+    if ((insideXData || insidetOff) && oldTextCode.match(/^}/)) {
+      insideXData = false;
+      insidetOff = false;
+    }
+
+    const textCode = outTestCode(trimmedLine);
+    if (textCode.match(/^s*(XData)\s*MessageMap/)) {
+      insideXData = true;
+    } else if (textCode.match(/^s*(XData|Storage)\s+\w+/)) {
+      insidetOff = true;
+    }
+
+    const [openXml, closeXml] = insideXData ? detectXml(textCode) : [0, 0];
+    const [openBrace, closeBrace] = detectBrace(textCode);
+
+    //const numberOpen = OpenBreak + OpenXdata;
+    const numberClosed = closeBrace + closeXml;
+
+    // Ajustar el nivel de indentación según las llaves de cierre
+    if (numberClosed && !(openXml && closeXml)) {
+      indentationLevel = indentationLevel - 1;
+    }
+    oldTextCode = textCode;
+    //oldTextLine = trimmedLine;
+  }
 
   for (let i = 0; i < document.lineCount; i++) {
     const line = document.lineAt(i);
     const trimmedLine = line.text.trim();
-
-    if (trimmedLine.match(/^(XData|Storage)\s+\w+/)) {
-      insideXData = true;
-    }
-    
-    let OpenXdata = 0;
-    let CloseXdata = 0;
-    if (insideXData){
-      OpenXdata = trimmedLine.match(/<[^>\/]+>/g)?.length || 0;
-      CloseXdata = trimmedLine.match(/<\/[^>]+>/g)?.length || 0;
-    }
-    let OpenBreak = trimmedLine.match(/\{/g)?.length || 0;
-    let CloseBreak = trimmedLine.match(/\}/g)?.length || 0;
-    
-    const numberOpen = OpenBreak + OpenXdata;
-    const numberClosed = CloseBreak + CloseXdata;
-
-    const formattedLine = insideXData ? trimmedLine : formaBaseLine(trimmedLine);
-
-    // Ajustar el nivel de indentación según las llaves de cierre
-    if (numberClosed && !(OpenXdata && CloseXdata)) {
-      indentationLevel = indentationLevel - 1;
-    }
-
-    // Ajustar la indentación adecuada
-    const indentedLine = addIdentado(formattedLine,indentationLevel);
-
-    
-    // Ajustar el nivel de indentación según las llaves de cierre
-    if (numberClosed && (OpenXdata && CloseXdata)) {
-      indentationLevel = indentationLevel - 1;
-    }
-
-    if (numberClosed > 1) {
-      indentationLevel = indentationLevel - numberClosed + 1;
-    }
-
-    // Ajustar el nivel de indentación según las llaves de apertura
-    if (numberOpen) {
-      indentationLevel++;
-    }
-    
-    if (insideXData && trimmedLine.match(/^}/)) {
-      insideXData = false;
-    }
+    calculateIndentation(trimmedLine);
+    const formattedLine = insideXData || insidetOff
+      ? trimmedLine
+      : formatBaseLine(trimmedLine);
+    const indentedLine = addIndentation(formattedLine, insidetOff ? 0 :indentationLevel);
 
     formattedLines.push(indentedLine);
   }
-  
-  return formattedLines;
-}
 
-const  clearCode =  (text: string):string =>{
-  let nuwText = text;
+  return formattedLines;
+};
+
+// Limpiar el código
+const clearCode = (text: string): string => {
+  let newText = text;
 
   //agregar saltos de liena cuando seencuentran }}
-  nuwText = nuwText.replace(/^([ \t]*)([^\}\n]*?)(}}+)/gm,(match, p1, p2, p3) => {
-    let text = p1 + p2;
-    let identationLevel = getIdentationLevel(p1)
-    text = p3.split('').reduce( (prev,dat,index)=>{
-      if (index == 0){
-        return  prev + dat;
-      }
-      identationLevel--;
-      return prev +"\n"+getTextIdent(identationLevel)+dat;
-    },text);
-    return text;
-  });
+  newText = newText.replace(
+    /^([ \t]*)([^\}\n]*?)(}}+)/gm,
+    (match, p1, p2, p3) => {
+      let text = p1 + p2;
+      let indentationLevel = getIndentationLevel(p1);
+      text = p3.split("").reduce((prev, dat, index) => {
+        if (index == 0) {
+          return prev + dat;
+        }
+        indentationLevel--;
+        return prev + "\n" + getTextIndent(indentationLevel) + dat;
+      }, text);
+      return text;
+    }
+  );
 
   // Quitar saltos de liena después de {
-  nuwText = nuwText.replace(/(\{)\n{2,}/g, '$1\n');
+  newText = newText.replace(/(\{)\n{2,}/g, "$1\n");
 
   // Quitar renglones vacíos antes de }
-  nuwText = nuwText.replace(/\n{2,}(\s*\})/g, '\n$1');
+  newText = newText.replace(/\n{2,}(\s*\})/g, "\n$1");
 
   // Eliminar saltos de línea consecutivos si hay más de uno
-  nuwText = nuwText.replace(/\n{3,}/g, '\n\n');
+  newText = newText.replace(/\n{3,}/g, "\n\n");
 
   // Eliminar saltos de línea dobles al final del documento, dejar solo uno
-  nuwText = nuwText.replace(/\n{2,}$/g, '\n');
+  newText = newText.replace(/\n{2,}$/g, "\n");
 
-  return nuwText;
-}
+  return newText;
+};
 
 // Función para formatear un documento
-export const formatDocument = (document: vscode.TextDocument): vscode.TextEdit[] => {
-  UpdateConfigBase();
+export const formatDocument = (
+  document: vscode.TextDocument
+): vscode.TextEdit[] => {
+  updateConfigBase();
   const edits: vscode.TextEdit[] = [];
   const lines = cleanAndFormatLines(document);
-  let formattedText = lines.join('\n');
+  let formattedText = lines.join("\n");
 
   // Agregar saltos de línea a los corchetes
-  formattedText = formattedText.replace(/^([^\n\/\/]*\{)/gm, '$1\n');
+  formattedText = formattedText.replace(/^([^\n\/\/]*\{)/gm, "$1\n");
 
   formattedText = clearCode(formattedText);
 
   // Reemplazar todo el contenido del documento con el texto formateado
   const lastLine = document.lineAt(document.lineCount - 1);
-  const range = new vscode.Range(0, 0, lastLine.range.end.line, lastLine.range.end.character);
+  const range = new vscode.Range(
+    0,
+    0,
+    lastLine.range.end.line,
+    lastLine.range.end.character
+  );
   edits.push(vscode.TextEdit.replace(range, formattedText));
 
   return edits;
